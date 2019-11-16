58c55ffe9873995407d7d669410d6b502b2f2ef6
MiguelGomez17 <gomezrenteriamiguelangel@gmail.com>
2019-11-16 09:41:28 -0600

    cargado del master

--- vacuna-digital/app/Http/Controllers/dataController.php
+++ vacuna-digital/app/Http/Controllers/dataController.php
@@ -9,7 +9,8 @@ use App\datos_identificacion;
 class dataController extends Controller
 {
     public function ingresar($curp){
-      $index = datos_identificacion::find($curp);
+      $index = datos_identificacion::Find($curp);
+      echo "<script>console.log('Debug Objects: " . $index . "' );</script>";
       return view("index",["index"=>$index]);
     }
 }
