646efbec2e1b4c8efc04467fd96f4408b81802c4
Luis Monreal <luismonreal84@gmail.com>
2019-11-15 18:32:16 -0600

    php

--- vacuna-digital/resources/views/EsquemaNino.php
+++ vacuna-digital/resources/views/EsquemaNino.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Esquema de vacunacion - Niños</title>
+    <title>Esquema de vacunación - Niños</title>
     <link rel="stylesheet" href="../../public/css/bootstrap.css">
     <script type="text/javascript" src="../../public/js/jquery.js"></script>
     <script type="text/javascript" src="../../public/js/bootstrap.js"></script>
